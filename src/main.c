205cd96290adba5b3c6fde034fe02bb3b28a2351
Henrik Hedelund <henrik@improove.se>
2019-06-01 02:26:41 +0200

    Memory partitioning scheme

--- src/main.c
+++ src/main.c
@@ -8,7 +8,18 @@
 
 #include "entry.h"
 
-int main (int argc, char **argv)
+typedef struct
+{
+  u32 size;
+  u32 nmemb;
+  u8 *base;
+} Bucket;
+
+static Bucket buckets[MAX_NUM_BUCKETS] = { 0 };
+static size_t num_buckets = 0;
+
+int
+main (int argc, char **argv)
 {
   (void) argc;
   (void) argv;
@@ -16,34 +27,70 @@ int main (int argc, char **argv)
   CacheEntryHashMap *entry_map = NULL;
 
   void  *memory = NULL;
-  size_t length = 0;
+  size_t total_memory_size = 0;
+  size_t total_bucket_size = 0;
 
   ////////////////////////////////////////
   // Calculate how much memory we need
-  length += sizeof (CacheEntryHashMap);
+  for (u32 size = MIN_BUCKET_SIZE, nmemb = MAX_BUCKET_ENTRY_COUNT;
+       size <= MAX_BUCKET_SIZE;
+       size <<= 1, nmemb >>= 1)
+    {
+      Bucket *bucket;
+      if (num_buckets >= MAX_NUM_BUCKETS)
+        break;
+
+      printf ("Adding bucket %u x %u Bytes\n", nmemb, size);
+
+      bucket = &buckets[num_buckets++];
+      bucket->size  = size;
+      bucket->nmemb = nmemb;
+      bucket->base  = NULL;
+
+      total_bucket_size += size * nmemb;
+    }
+  printf ("Total bucket memory is %lu\n", total_bucket_size);
+
+  total_memory_size = (total_bucket_size
+                       + sizeof (CacheEntryHashMap));
 
   ////////////////////////////////////////
   // Try to allocate memory
-  printf ("Reserving %lu bytes\n", length);
-  memory = mmap ((void *) 0, length, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+  printf ("Reserving %lu bytes\n", total_memory_size);
+  memory = mmap ((void *) 0, total_memory_size,
+                 PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
   if (memory == MAP_FAILED)
     {
-      fprintf (stderr, "Failed to map %lu bytes: %s\n", length, strerror (errno));
+      fprintf (stderr, "Failed to map %lu bytes: %s\n",
+               total_memory_size, strerror (errno));
       return EXIT_FAILURE;
     }
 
-  printf ("Committing %lu bytes\n", length);
-  if (0 != mprotect (memory, length, PROT_READ | PROT_WRITE))
+  printf ("Committing %lu bytes\n", total_memory_size);
+  if (0 != mprotect (memory, total_memory_size, PROT_READ | PROT_WRITE))
     {
-      fprintf (stderr, "Failed to commit %lu bytes: %s\n", length, strerror (errno));
+      fprintf (stderr, "Failed to commit %lu bytes: %s\n",
+               total_memory_size, strerror (errno));
       return EXIT_FAILURE;
     }
 
   ////////////////////////////////////////
   // ... Profit
 
-  entry_map = memory;
+  void *next_memory_location = memory;
+  entry_map = next_memory_location;
+  next_memory_location += sizeof (CacheEntryHashMap);
   init_cache_entry_map (entry_map);
+
+  for (u32 b = 0; b < num_buckets; ++b)
+    {
+      Bucket *bucket = &buckets[b];
+      size_t bucket_size = bucket->size * bucket->nmemb;
+      bucket->base = next_memory_location;
+      next_memory_location += bucket_size;
+    }
+
+  assert ((size_t) (next_memory_location - memory) == total_memory_size);
 
   {
     CacheEntry *entry = NULL;
@@ -67,7 +114,7 @@ int main (int argc, char **argv)
     set_cache_entry (entry_map,
                      (u8 *) mykey1, strlen (mykey1),
                      (u8 *) myval1, strlen (myval1));
-    
+
     entry = lock_and_get_cache_entry (entry_map, (u8 *) "mykey", strlen ("mykey"));
     assert (entry == NULL);
     entry = lock_and_get_cache_entry (entry_map, (u8 *) "mykey1", strlen ("mykey1"));
@@ -91,9 +138,10 @@ int main (int argc, char **argv)
 
   ////////////////////////////////////////
   // Clean up
-  if (0 != munmap (memory, length))
+  if (0 != munmap (memory, total_memory_size))
     {
-      fprintf (stderr, "Failed to unmap %lu bytes: %s\n", length, strerror (errno));
+      fprintf (stderr, "Failed to unmap %lu bytes: %s\n",
+               total_memory_size, strerror (errno));
       return EXIT_FAILURE;
     }
 
